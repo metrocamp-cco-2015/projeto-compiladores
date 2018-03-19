513eb0d1bbee1413a09afffbd9b5c3026114b100
Ednaldo Leite Junior <ednaldo@ciandt.com>
2018-03-19 13:57:08 -0300

    Addin ENUMs into TokenType

--- Java/src/main/java/utils/TokenType.java
+++ Java/src/main/java/utils/TokenType.java
@@ -2,4 +2,9 @@ package src.main.java.utils;
 
 public enum TokenType {
 
+    NUM_INT, NUM_FLOAT, LITERAL, ID, RELOP, ADDSUB, MULTDIV,
+    ATTRIB, TERM, L_PAR, R_PAR, LOGIC_VAL, LOGIC_OP, TYPE,
+    PROGRAM, END_PROG, BEGIN, END, IF, THEN, ELSE, FOR,
+    WHILE, DECLARE, TO
+
 }
